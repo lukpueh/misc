8a7d6c4793c48bdd98ae3c762e944ad11cccdcd2
Lukas P <luk.puehringer@gmail.com>
2016-09-14 15:43:42 -0400

    Adds python lib res/raw unpacking to Main Activity

--- jni-hello-android-child/app/src/main/java/com/example/lukp/helloandroidjni/MainActivity.java
+++ jni-hello-android-child/app/src/main/java/com/example/lukp/helloandroidjni/MainActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
 
     static String TAG = "Main";
@@ -12,6 +14,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String FILES_ROOT = getApplicationContext().getFilesDir().getPath() + "/";
+
+        Log.i(TAG, String.format("Unpacking python archive to %s", FILES_ROOT));
+        try {
+            Utils.unzip(getResources().openRawResource(R.raw.python_lib), FILES_ROOT, true);
+        } catch (IOException e) {
+            Log.i(TAG, "Couldn't unpack python archive");
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         Log.i(TAG, String.format("Before Activity starts MyService1 in Activity -- PID: %d",
                 android.os.Process.myPid()));
